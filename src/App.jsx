db79a9e4ffa9b74fa3c4590d6dc738dfaf2ff48a
saphaltaneupane <neupanesaphalta@gmail.com>
2025-11-18 12:51:23 +0545

    starting

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Landing } from "./components/Landing/landing";
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Landing/landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <div>
+      <div>
+        <h1>Stargram</h1>
+      </div>
+    </div>
+  );
+};
